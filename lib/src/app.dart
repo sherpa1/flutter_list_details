1a779cb5425d9e9ac11462fa361f88ec17196054
Sherpa <alex@sherpa.one>
2022-02-07 08:50:41 +0100

    todo : fix error

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//TODO: fix import error given by default by template files
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
